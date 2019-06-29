d3b45c6c05626aca0659f6b6b5ac90fc0435092e
Piotr Gankiewicz <piotr.gankiewicz@gmail.com>
2019-06-29 21:03:46 +0200

    Removed onError

--- src/Convey.MessageBrokers/IBusSubscriber.cs
+++ src/Convey.MessageBrokers/IBusSubscriber.cs
@@ -1,12 +1,11 @@
 ﻿using System;
 using System.Threading.Tasks;
-using Convey.Types;
 
 namespace Convey.MessageBrokers
 {
     public interface IBusSubscriber
     {
-        IBusSubscriber Subscribe<TMessage>(Func<IServiceProvider, TMessage, ICorrelationContext, Task> handle,
-            Func<TMessage, ConveyException, object> onError = null) where TMessage : class;
+        IBusSubscriber Subscribe<TMessage>(Func<IServiceProvider, TMessage, ICorrelationContext, Task> handle)
+            where TMessage : class;
     }
 }
