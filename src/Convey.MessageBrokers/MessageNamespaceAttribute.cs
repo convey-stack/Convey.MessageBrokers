e3351bccc2c1fc4c1fae23b0f8a06a881bc9e1b0
Piotr Gankiewicz <piotr.gankiewicz@gmail.com>
2019-06-30 13:43:37 +0200

    Key and namespace

--- src/Convey.MessageBrokers/MessageNamespaceAttribute.cs
+++ src/Convey.MessageBrokers/MessageNamespaceAttribute.cs
@@ -6,11 +6,13 @@ namespace Convey.MessageBrokers
     public class MessageNamespaceAttribute : Attribute
     {
         public string Namespace { get; }
+        public string Key { get; }
         public bool External { get; }
 
-        public MessageNamespaceAttribute(string @namespace, bool external = true)
+        public MessageNamespaceAttribute(string @namespace, string key = null, bool external = true)
         {
             Namespace = @namespace?.ToLowerInvariant();
+            Key = key;
             External = external;
         }
     }
