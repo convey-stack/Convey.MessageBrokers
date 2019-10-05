146126ae7da23b6d7c31ff82987066bbef3e7888
Piotr Gankiewicz <piotr.gankiewicz@gmail.com>
2019-10-05 22:32:38 +0200

    Additional attributes

--- src/Convey.MessageBrokers/MessageAttribute.cs
+++ src/Convey.MessageBrokers/MessageAttribute.cs
@@ -9,15 +9,21 @@ namespace Convey.MessageBrokers
         public string RoutingKey { get; }
         public string Queue { get; }
         public string ExchangeType { get; }
+        public bool DeclareExchange { get; }
+        public bool DurableExchange { get; }
+        public bool AutoDeleteExchange { get; }
         public bool External { get; }
 
-        public MessageAttribute(string exchange = null, string routingKey = null, string queue = null,
-            string exchangeType = null,  bool external = false)
+        public MessageAttribute(string exchange, string routingKey, string queue, string exchangeType,
+            bool declareExchange, bool durableExchange, bool autoDeleteExchange, bool external)
         {
             Exchange = exchange;
             RoutingKey = routingKey;
             Queue = queue;
             ExchangeType = exchangeType;
+            DeclareExchange = declareExchange;
+            DurableExchange = durableExchange;
+            AutoDeleteExchange = autoDeleteExchange;
             External = external;
         }
     }
