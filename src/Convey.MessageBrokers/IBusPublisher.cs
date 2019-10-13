c3f79fd6fa2cd3b8676efdad263e1a4462645ba1
Piotr Gankiewicz <piotr.gankiewicz@gmail.com>
2019-10-13 18:39:20 +0200

    Interfaces rename

--- src/Convey.MessageBrokers/IBusPublisher.cs
+++ src/Convey.MessageBrokers/IBusPublisher.cs
@@ -2,7 +2,7 @@ using System.Threading.Tasks;
 
 namespace Convey.MessageBrokers
 {
-    public interface IPublisher
+    public interface IBusPublisher
     {
         Task PublishAsync<T>(T message, string messageId = null, string correlationId = null,
             object context = null) where T : class;
--- src/Convey.MessageBrokers/MessageAttribute.cs
+++ src/Convey.MessageBrokers/MessageAttribute.cs
@@ -8,23 +8,14 @@ namespace Convey.MessageBrokers
         public string Exchange { get; }
         public string RoutingKey { get; }
         public string Queue { get; }
-        public string ExchangeType { get; }
-        public bool DeclareExchange { get; }
-        public bool DurableExchange { get; }
-        public bool AutoDeleteExchange { get; }
         public bool External { get; }
 
         public MessageAttribute(string exchange = null, string routingKey = null, string queue = null,
-            string exchangeType = null, bool declareExchange = false, bool durableExchange = false,
-            bool autoDeleteExchange = false, bool external = false)
+            bool external = false)
         {
             Exchange = exchange;
             RoutingKey = routingKey;
             Queue = queue;
-            ExchangeType = exchangeType;
-            DeclareExchange = declareExchange;
-            DurableExchange = durableExchange;
-            AutoDeleteExchange = autoDeleteExchange;
             External = external;
         }
     }
